c915af0573f1f834907d22390763053dac398df0
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-01-30 13:43:49 +0530

    setting up the ai to detect the authenticity of images

--- Backend/detective.py
+++ Backend/detective.py
@@ -1,1 +1,19 @@
 # this file for fake or real check of images
+# import roboflow sdk to commuicate with cloud based AI model
+from roboflow import Roboflow
+import os
+
+# 1.replace with actual privae key from roboflow
+
+# there is a private security key found in roboflow deploy it act as your password
+API_KEY="c5jEmDo5cIPP9Ap3LNJl"
+# create a connection
+rf = Roboflow(api_key=API_KEY)
+
+# use project name and version number from ss
+project=rf.workspace.project("gov_ai_compliant")
+version=project.version(1).model
+
+# 3.send the image to roboflow and returns detection result
+def run_ai(image_path):
+    prediction=model.predict(image_path,confidence=40).json()
